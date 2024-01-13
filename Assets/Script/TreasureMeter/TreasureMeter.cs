e6c1d84129bf13ed8efcf0f0ce51eb27ed5e9a3d
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-01-13 11:24:48 +0900

    [fix]近くの宝に反応するように修正した

--- Assets/Script/TreasureMeter/TreasureMeter.cs
+++ Assets/Script/TreasureMeter/TreasureMeter.cs
@@ -9,7 +9,8 @@ public class TreasureMeter : MonoBehaviour
     [SerializeField] GameObject _needle;
     Transform _needleTransform;
     [SerializeField] GameObject _player;
-    [SerializeField] GameObject _treasure; //お宝
+    GameObject _treasure; //お宝
+    [SerializeField, Header("宝のタグ名")] string _treasureTagName;
     [SerializeField] float _maxDis = 20; //レーダーが反応する最大距離
     [SerializeField] float _mostCloseDis = 2; //レーダーが反応する最も近い距離
     float _currentDistance;
@@ -24,9 +25,12 @@ public class TreasureMeter : MonoBehaviour
         _needleTransform.eulerAngles = new Vector3(0, 0, GetNeedleRotation());
     }
 
+    /// <summary>メータの針の角度を決めるメソッド</summary>
+    /// <returns>Z軸rotationの数値</returns>
     float GetNeedleRotation()
     {
         float totalAngleSize = _zeroMeterAngle - _maxMeterAngle;
+        NearObjectWithTag(_treasureTagName);
         _currentDistance = Vector3.Distance(_player.transform.position, _treasure.transform.position);
         if (_currentDistance > _maxDis)
             return _zeroMeterAngle;
@@ -37,5 +41,32 @@ public class TreasureMeter : MonoBehaviour
         float distanceNormalized = 1 - _currentDistance / _maxDis;
         //float distanceNormalized = _currentDis / _treasureDis;
         return _zeroMeterAngle - distanceNormalized * totalAngleSize;
+    }
+
+    /// <summary>指定したタグのオブジェクトの中から一番近いゲームオブジェクトを設定する </summary>
+    /// <param name="tagName">調べたいタグ</param>
+    void NearObjectWithTag(string tagName)
+    {
+        //該当オブジェクトが一つしかない場合それを返す
+        GameObject[] targets = GameObject.FindGameObjectsWithTag(tagName);
+        if(targets.Length == 1)
+        {
+            _treasure = targets[0];
+            return;
+        }
+
+        GameObject result = null;
+        float minTargetDis = float.MaxValue;
+        foreach(var target in targets)
+        {
+            float targetaDis = Vector3.Distance(_player.transform.position, target.transform.position);
+            if(targetaDis < minTargetDis)
+            {
+                minTargetDis = targetaDis;
+                result = target;
+            }
+        }
+        //一番近くにあるオブジェクトを設定する
+        _treasure = result;
     }
 }
