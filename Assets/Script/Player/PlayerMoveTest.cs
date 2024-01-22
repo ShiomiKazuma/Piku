0f7e8a9dfe12708189162143d757207441c2bafc
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-01-22 17:03:49 +0900

    [fix]プレイヤー移動を修正

--- Assets/Script/Player/PlayerMoveTest.cs
+++ Assets/Script/Player/PlayerMoveTest.cs
@@ -9,6 +9,8 @@ public class PlayerMoveTest : MonoBehaviour
     [SerializeField] float _moveSpeed = 10;
     Quaternion _targetRotation;
     [SerializeField, Header("振り向く速さ")] float _rotateSpeed = 10f;
+    [SerializeField, Header("接地判定用のオブジェクト")] Transform _groundCheckTrn;
+    bool IsGround = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +22,7 @@ public class PlayerMoveTest : MonoBehaviour
     {
         _h = Input.GetAxisRaw("Horizontal");
         _v = Input.GetAxisRaw("Vertical");
+        CheckGround();
     }
 
     private void FixedUpdate()
@@ -36,5 +39,23 @@ public class PlayerMoveTest : MonoBehaviour
         }
         transform.rotation = Quaternion.RotateTowards(transform.rotation, _targetRotation, _rotateSpeed);
         _rb.velocity = dir * _moveSpeed;
+    }
+
+    /// <summary>
+    /// 接地判定
+    /// </summary>
+    void CheckGround()
+    {
+        //接地判定
+        IsGround = Physics.Raycast(_groundCheckTrn.position, Vector3.down, _groundCheckRayLength, _whatIsGround);
+        //IsGround = Physics.Raycast(transform.position, Vector3.down, _playerHeight * 0.5f + 0.2f, _groundLayer);
+        if (IsGround)
+        {
+            _rb.drag = _groundDrag;
+        }
+        else
+        {
+            _rb.drag = 0;
+        }
     }
 }
