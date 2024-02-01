964011ec26ed191ac99342a201de4ea53a29668e
ShiomiKazuma <kazu_shio_1217@icloud.com>
2024-02-01 23:51:13 +0900

    [fix]ピクミンの呼びだし処理を修正

--- Assets/Script/Pikimin/PikiminCall.cs
+++ Assets/Script/Pikimin/PikiminCall.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class PikiminCall : MonoBehaviour
 {
     [SerializeField, Header("笛の半径")] float _radius = 5.0f;
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("よんでいる");
+            CallPikmin();
+        }
+    }
     public void CallPikmin()
     {
+        //RaycastHit[] hits = Physics.SphereCastAll(this.transform.position, _radius);
         var pikmin = Physics.OverlapSphere(this.transform.position, _radius);
         foreach(var p in pikmin)
         {
+            var pikg = p.gameObject;
             //ピクミンなら隊列に加わる
+            Pikmin pik = pikg.GetComponent<Pikmin>();
+            if (pik != null)
+            {
+                Debug.Log("ピクミンが呼ばれた");
+            }
+            
         }
     }
 }
